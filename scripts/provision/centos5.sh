f0102d68d7553de1c38a527fd03909f92df9dac3
Paul Ewing <pcewing00@gmail.com>
2017-02-06 09:47:57 -0800

    Initial improvements
    To make the provisioning process more consistent, I've added a base provision script. It will source an "Implementation"
    script. The implementation scripts provide expected functions with distro-specific instructions.
    
    
    Former-commit-id: 7fe256e84e5e4139604367db9ff0ef939cfed234

---
+++ scripts/provision/centos5.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "Centos5 support is still under construction."
+exit 1
--- scripts/provision/ubuntu.sh
+++ scripts/provision/ubuntu.sh
@@ -25,72 +25,91 @@ upgrade(){ echo "Upgrading packages... "; try sudo apt-get -y upgrade; }
 install(){ echo "Installing $1... "; try sudo apt-get -y install $1; }
 add-apt-repository(){ echo "Adding $1 repository... "; try sudo add-apt-repository -y ppa:$1; }
 
-section(){
-  echo -en '\n'
-  echo "$1"
-  echo "==============================================================="
+install_basics()
+{
+  update
+  install git
+  install wget
+  install curl
+  install make
+  install xclip
+  install ack-grep
+  install build-essential
+  install tmux
+  install inotify-tools
 }
 
-# Update and install basic tools
-section "Installing the Basics"
-update
-install git
-install wget
-install curl
-install make
-install xclip
-install ack-grep
-install build-essential
-install tmux
-install zsh
-install inotify-tools
-add-apt-repository neovim-ppa/unstable
-update
-install neovim
-install python-dev
-install python-pip
-install python3-dev
-install python3-pip
-echo "Setting up neovim python support..."
-try pip install --upgrade neovim
-echo "Setting up neovim python3 support..."
-try pip3 install --upgrade neovim
+install_python()
+{
+  install python-dev
+  install python-pip
+  install python3-dev
+  install python3-pip
+}
 
-if [[ $remote != true ]]; then
+install_shell()
+{
+  install zsh
+
+  echo "Creating the $HOME/.zsh directory if it doesn't exist...  "
+  try mkdir -p $HOME/.zsh
+  echo "Downloading antigen version ${antigen_version}..."
+  try eval $(curl https://cdn.rawgit.com/zsh-users/antigen/v${antigen_version}/bin/antigen.zsh > $HOME/.zsh/antigen.zsh)
+}
+
+install_neovim()
+{
+  add-apt-repository neovim-ppa/unstable
+  update
+  install neovim
+
+  echo "Setting up neovim python support..."
+  try pip install --upgrade neovim
+  echo "Setting up neovim python2 support..."
+  try pip2 install --upgrade neovim
+  echo "Setting up neovim python3 support..."
+  try pip3 install --upgrade neovim
+
+  echo "Creating the $HOME/.config/nvim/autoload directory if it doesn't exist...  "
+  try mkdir -p $HOME/.config/nvim/autoload
+  echo "Downloading vim-plug from https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim...  "
+  try curl -fLo $HOME/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+}
+
+install_nodejs()
+{
+  echo "Adding NodeJS repository... "
+  try sudo bash -c "curl --silent --location https://deb.nodesource.com/setup_6.x | bash -"
+  install nodejs
+  echo "Creating the $HOME/.npm-global directory if it doesn't exist...  "
+  try mkdir -p $HOME/.npm-global
+  echo "Setting npm global directory to $HOME/.npm-global to avoid permissions issues when globally installing packages...  "
+  try npm config set prefix "$HOME/.npm-global"
+}
+
+install_npm_packages()
+{
+  echo "Installing diff-so-fancy... "
+  try npm install -g diff-so-fancy
+}
+
+install_terminal_emulator()
+{
   install rxvt-unicode
   install rxvt-unicode-256color
-  install i3
+}
 
-  section "Installing Powerline Derivative Fonts"
+install_graphical_environment()
+{
+  install i3
+}
+
+install_fonts()
+{
   echo "Cloning powerline/fonts to ~/powerline_fonts... "
   try git clone https://github.com/powerline/fonts ~/powerline_fonts
   echo "Executing ~/powerline_fonts/install.sh..."
   try ~/powerline_fonts/install.sh
   echo "Cleaning up ~/powerline_fonts directory... "
   try rm -rf ~/powerline_fonts
-fi
-
-section "Installing Antigen"
-echo "Creating the $HOME/.zsh directory if it doesn't exist...  "
-try mkdir -p $HOME/.zsh
-echo "Downloading antigen version ${antigen_version}..."
-try eval $(curl https://cdn.rawgit.com/zsh-users/antigen/v${antigen_version}/bin/antigen.zsh > $HOME/.zsh/antigen.zsh)
-
-section "Installing Vim-Plug"
-echo "Creating the $HOME/.config/nvim/autoload directory if it doesn't exist...  "
-try mkdir -p $HOME/.config/nvim/autoload
-echo "Downloading vim-plug from https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim...  "
-try curl -fLo $HOME/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-section "Installing NodeJS"
-echo "Adding NodeJS repository... "
-try sudo bash -c "curl --silent --location https://deb.nodesource.com/setup_6.x | bash -"
-install nodejs
-echo "Creating the $HOME/.npm-global directory if it doesn't exist...  "
-try mkdir -p $HOME/.npm-global
-echo "Setting npm global directory to $HOME/.npm-global to avoid permissions issues when globally installing packages...  "
-try npm config set prefix "$HOME/.npm-global"
-
-section "Installing Global NPM Packages"
-echo "Installing diff-so-fancy... "
-try npm install -g diff-so-fancy
+}
