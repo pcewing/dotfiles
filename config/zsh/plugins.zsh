c8988f11ea849ba97d69879db6a7a64883b1b817
Paul Ewing <pcewing00@gmail.com>
2017-05-15 14:06:55 -0700

    Start creating custom prompt
    Former-commit-id: 4105a6c6b4bbdc734c334681f68ddc30370117d8

--- config/zsh/plugins.zsh
+++ config/zsh/plugins.zsh
@@ -4,18 +4,8 @@ source $HOME/.zsh/antigen.zsh
 antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle zsh-users/zsh-autosuggestions
 
-# Themes
-antigen theme bhilburn/powerlevel9k powerlevel9k
-
 antigen apply
 
 # Plugin configuration
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=10'
 
-# Sometimes turning off version control is useful because it can slow down
-# the shell significantly
-#POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(time dir)
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(time dir vcs)
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status background_jobs)
-POWERLEVEL9K_SHORTEN_STRATEGY="truncate_middle"
-POWERLEVEL9K_SHORTEN_DIR_LENGTH=2
