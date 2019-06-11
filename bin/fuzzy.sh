7cb45c57ce2620cff4d697ff5b804cc3d403f909
Paul Ewing <pcewing00@gmail.com>
2019-06-11 09:23:52 -0700

    Updates
    Former-commit-id: 5cc7c803660b12d0b9973dd67c7ad51fde8699bc [formerly 4f5d4299fc7f9a924a368c9f5b39fe288a3a7502] [formerly 5a7e88744d43f1c9c6154a0fb8897b53b849b8be [formerly 5534c79d7ce346e05ef9f105d08d19dd0358db13]]
    Former-commit-id: 6a44b1d04372e4ea5f5a59eb47192572a9a8db43 [formerly fe61bbdfd83c932d0e6c0f6c127fc8e1f7e79f35]
    Former-commit-id: a45427c9726d0474a2cebdae324621c49b30f959

---
+++ bin/fuzzy.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+action="$1"
+
+if [ -z "$action" ]; then
+    echo "Usage: ./fuzzy.sh <action>" 1>&2
+    exit 1
+fi
+
+directories="$HOME
+$HOME/box/pic/screenshots
+$HOME/src
+$HOME/box/doc
+$HOME/box/pic
+$HOME/box/mus
+$HOME/box/vid
+$HOME/Desktop
+$HOME/Downloads
+$HOME/Templates
+$HOME/Public
+$HOME/Documents
+$HOME/Music
+$HOME/Pictures
+$HOME/Videos"
+
+function fuzzy_nautilus() {
+    selection="$(echo "$directories" | rofi -dmenu)"
+
+    if [ ! -z "$selection" ]; then
+        nautilus "$selection" > /dev/null 2>&1 &
+    fi
+}
+
+function fuzzy_ranger() {
+    selection="$(echo "$directories" | rofi -dmenu)"
+
+    if [ ! -z "$selection" ]; then
+        urxvt -e bash -c "ranger \"$selection\""
+    fi
+}
+
+if [ "$action" = "nautilus" ]; then
+    fuzzy_nautilus
+    exit 0
+elif [ "$action" = "ranger" ]; then
+    fuzzy_ranger
+    exit 0
+fi
+
+echo "ERROR: Unknown action \"$action\"" 1>&2
+exit 1
+
