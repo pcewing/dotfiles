fb0368bb78976c03b0bcdd922df0568e0d59423c
Paul Ewing <pewing@blizzard.com>
2020-08-11 08:54:22 -0700

    Update fssh script

--- bin/fssh.sh
+++ bin/fssh.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
-known_hosts="$(cat "$HOME/.ssh/known_hosts" | sed -e 's/,.*$//')"
+function yell () { >&2 echo "$*";  }
+function die () { yell "$*"; exit 1; }
 
-selection="$(echo "$known_hosts" | fzf)"
+fssh_hosts_file="$HOME/.fssh_hosts"
+
+if [ ! -f "$fssh_hosts_file" ]; then
+    die "ERROR: Hosts file \"$fssh_hosts_file\" does not exist!"
+fi
+
+fssh_hosts="$(cat "$HOME/.fssh_hosts" | grep -vP '(^ *$)|(^#.*)')"
+
+selection="$(echo "$fssh_hosts" | fzf)"
 
 if [ ! -z "$selection" ]; then
     ssh "$selection"
