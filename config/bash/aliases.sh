ff51871a21f48da96b2f4ae76cde0a516992f2cb
Paul Ewing <pewing@blizzard.com>
2023-12-19 08:52:33 -0800

    More alias improvements

--- config/bash/aliases.sh
+++ config/bash/aliases.sh
@@ -56,13 +56,19 @@ set_alias '0' 'lss'     'ls --sort=extension --group-directories-first'
 # Helpers
 set_alias '1' 'grep' 'grep --color=auto'
 
+# I can never remember where core dumps are by default on Ubuntu
+# TODO: We should probably make a function for this so we can do something more powerful. Like:
+# - Pipe the list of core files into FZF
+# - If one is selected, open it with GDB
+set_alias '0' 'cores' 'echo -e "/var/lib/apport/coredump\n" && ls /var/lib/apport/coredump -l'
+
 # Make disk free always human-readable
-if _is_installed 'df' ; then
+if _is_installed 'df'; then
     set_alias '1' 'df' 'df -h'
 fi
 
 # Sorted, human-readable disk usage by depth
-if _is_installed 'du' ; then
+if _is_installed 'du'; then
     set_alias '0' 'du1' 'du -hd1 2>/dev/null | sort -hr'
     set_alias '0' 'du2' 'du -hd2 2>/dev/null | sort -hr'
     set_alias '0' 'du3' 'du -hd3 2>/dev/null | sort -hr'
@@ -71,19 +77,19 @@ if _is_installed 'du' ; then
 fi
 
 # Ranger aliases
-if _is_installed 'ranger' ; then
+if _is_installed 'ranger'; then
     set_alias '0' 'r'       'ranger'
     set_alias '0' 'notes'   'ranger ~/notebook'
 fi
 
 # Locate aliases
-if _is_installed 'locate' ; then
+if _is_installed 'locate'; then
     set_alias '0' 'lv' 'locate --regex'
     set_alias '0' 'lV' 'locate'
 fi
 
 # Neovim aliases
-if _is_installed 'nvim' ; then
+if _is_installed 'nvim'; then
     set_alias '0' 'nvimf'       'nvim $(fzf)'
     set_alias '0' 'nvimd'       'nvim $DOTFILES'
     set_alias '0' 'nvimt'       'nvim $(mktemp)'
@@ -91,13 +97,13 @@ if _is_installed 'nvim' ; then
 fi
 
 # Apt aliases
-if _is_installed 'apt' ; then
+if _is_installed 'apt'; then
     set_alias '0' 'apti' 'sudo apt install -y'
     set_alias '0' 'apts' 'sudo apt search'
 fi
 
 # Pacman aliases
-if _is_installed 'pacman' ; then
+if _is_installed 'pacman'; then
     set_alias '0' 'pinstall'    'sudo pacman -Syu --noconfirm'
     set_alias '0' 'psearch'     'sudo pacman -Ss'
     set_alias '0' 'premove'     'sudo pacman -R --noconfirm'
@@ -105,13 +111,13 @@ if _is_installed 'pacman' ; then
 fi
 
 # Wi-Fi aliases
-if _is_installed 'nmcli' ; then
+if _is_installed 'nmcli'; then
     set_alias '0' 'wifisearch' 'nmcli device wifi list'
     set_alias '0' 'wificonnect' 'nmcli device wifi connect --ask'
 fi
 
 # Git aliases
-if _is_installed 'git' ; then
+if _is_installed 'git'; then
     set_alias '1' 'gs'      'git status --short'
     set_alias '0' 'gc'      'git commit'
     set_alias '0' 'gco'     'git checkout'
@@ -127,56 +133,57 @@ if _is_installed 'git' ; then
 fi
 
 # SVN aliases
-if _is_installed 'svn' ; then
-    set_alias '1' 'ss' 'svn status'
-    set_alias '0' 'sd' 'svn diff --diff-cmd=bcompare_svn'
-    set_alias '0' 'sl' 'svn log -l'
+if _is_installed 'svn'; then
+    set_alias '1' 'ss'  'svn status'
+    set_alias '0' 'sd'  'svn diff'
+    set_alias '0' 'sdt' 'svn diff --diff-cmd=bcompare_svn'
+    set_alias '0' 'sl'  'svn log -l'
 fi
 
 # Tmux aliases
-if _is_installed 'tmux' ; then
+if _is_installed 'tmux'; then
     set_alias '0' 'tm'  'tmux'
     set_alias '0' 'tms' "tmux new-session -s \"\$(basename \"\$(pwd)\")\""
     set_alias '0' 'tma' 'tmux attach'
 fi
 
-if _is_installed 'terraform' ; then
+if _is_installed 'terraform'; then
     set_alias '0' 'tf' 'terraform'
 fi
 
-if _is_installed 'consul' ; then
+if _is_installed 'consul'; then
     set_alias '0' 'csl' 'consul'
 fi
 
 # Restart mpd
-if _is_installed 'mpd' ; then
+if _is_installed 'mpd'; then
     set_alias '0' 'mpd_restart' 'mpd --kill; mpd'
 fi
 
 # Restart wpr
-if _is_installed 'wpr' ; then
+if _is_installed 'wpr'; then
     set_alias '0' 'wpr_restart' 'killall wpr &>/dev/null; wpr &>/dev/null &'
 fi
 
 # Other
-if _is_installed 'startx' ; then
+if _is_installed 'startx'; then
     set_alias '0' 'sx' 'startx'
 fi
 
-if _is_installed 'xrdb' ; then
+if _is_installed 'xrdb'; then
     set_alias '0' 'reload_xresources' 'xrdb -merge ~/.Xresources'
 fi
 
-if _is_installed 'xclip' ; then
+if _is_installed 'xclip'; then
     set_alias '0' 'clip' 'xclip -i -selection clipboard'
 fi
 
-if _is_installed 'gnome-control-center' ; then
+if _is_installed 'gnome-control-center'; then
     set_alias '0' 'gnome-settings' 'env XDG_CURRENT_DESKTOP=GNOME gnome-control-center'
 fi
 
 # Docker utilities
-if _is_installed 'docker' ; then
+if _is_installed 'docker'; then
     set_alias '0' 'dps' 'docker_pss'
 
     # Stop all docker containers
@@ -193,12 +200,12 @@ if _is_installed 'docker' ; then
     # Stop and remove all docker containers and then remove all docker images
     set_alias '0' 'docker_nuke' 'docker_stop; docker_rma; docker_rmi'
 
-    if ! _is_installed 'iex' ; then
+    if ! _is_installed 'iex'; then
         set_alias '0' 'iex' 'docker run -it elixir:latest iex'
     fi
 fi
 
-if _is_installed 'python' ; then
+if _is_installed 'python'; then
     set_alias '0' 'serve' 'python -m SimpleHTTPServer'
 fi
 
@@ -210,6 +217,6 @@ if grep 'DISTRIB_ID=Ubuntu' /etc/lsb-release &>/dev/null; then
 fi
 
 # Parsec
-if _is_installed 'parsecd' ; then
+if _is_installed 'parsecd'; then
     set_alias '0' 'parsec' 'parsecd app_daemon=1'
 fi
