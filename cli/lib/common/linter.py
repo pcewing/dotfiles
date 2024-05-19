7f0535245886834c462e8c0868da1f61b2b96d7e
Paul Ewing <pcewing00@gmail.com>
2024-05-19 13:30:22 -0700

    Implement lint CLI command

--- cli/lib/common/linter.py
+++ cli/lib/common/linter.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
+import hashlib
+import os
 import re
+import shutil
 
 from lib.common.dir import Dir
 from lib.common.file_walker import FileWalker
 from lib.common.log import Log
-from lib.common.util import sh
+from lib.common.util import Util, sh
 
 
 class Linter:
@@ -13,6 +16,9 @@ class Linter:
     def lint(files: list[str]) -> None:
         if len(files) == 0:
             files = Linter._get_python_files()
+
+        Util.rmdir(Linter._tmp_dir())
+        os.makedirs(Linter._tmp_dir())
 
         for file in files:
             Linter._lint(file)
@@ -27,14 +33,8 @@ class Linter:
 
     @staticmethod
     def _lint(file: str) -> None:
-        # TODO: Implement this
-        # - Make sure file is tidy
-        #     - Probably easiest to just make a copy of the file, run tidy on the
-        #       copy, then compare against the original
-        #     - If there are differences, fail
-        # - Static type checking
-        #     - `mypy --config-file "$DOTFILES/mypy.ini" ...`
-        raise Exception("Not yet implemented")
+        Linter._ensure_tidy(file)
+        Linter._ensure_static_typing(file)
 
     @staticmethod
     def _tidy(file: str, dry_run: bool) -> None:
@@ -82,3 +82,28 @@ class Linter:
         Log.info("formatting file", [("file", file)])
         if not dry_run:
             sh(["black", file], check=True)
+
+    @staticmethod
+    def _ensure_tidy(file: str) -> None:
+        dst = os.path.join(Linter._tmp_dir(), os.path.basename(file))
+        shutil.copyfile(file, dst)
+        Linter.tidy([dst], False)
+        if Linter._file_md5(file) != Linter._file_md5(dst):
+            raise Exception(f"File is not tidy: {file}")
+
+    @staticmethod
+    def _ensure_static_typing(file: str) -> None:
+        mypy_cmd = ["mypy", "--config-file", os.path.join(Dir.dot(), "mypy.ini"), file]
+        if sh(mypy_cmd) != 0:
+            raise Exception(f"File does not have correct static type hints: {file}")
+
+    @staticmethod
+    def _tmp_dir() -> str:
+        return os.path.join(Dir.tmp(), "linter")
+
+    @staticmethod
+    def _file_md5(file: str) -> str:
+        md5 = hashlib.md5()
+        with open(file, "rb") as f:
+            md5.update(f.read())
+        return md5.hexdigest()
