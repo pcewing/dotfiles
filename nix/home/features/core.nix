55c3e5e51565e766748788770f19b0a270ce3c06
Paul Ewing <pcewing00@gmail.com>
2026-01-02 00:00:26 -0800

    Progress, primarily neovim plugins

--- nix/home/features/core.nix
+++ nix/home/features/core.nix
@@ -96,7 +96,80 @@ in
   ];
 
   programs.git.enable = true;
-  programs.neovim.enable = true;
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  
+    plugins = with pkgs.vimPlugins; [
+      fzf-vim
+  
+      telescope-nvim
+      plenary-nvim
+      popup-nvim
+  
+      nvim-treesitter.withAllGrammars
+      # TODO: If we want to trim this down, we could do something like this:
+      #nvim-treesitter.withPlugins (p: [ p.c p.java ]))
+      # And copy what we used to have in our treesitter.lua file. But just installing all grammars is fine for now.
+  
+      ultisnips
+      vim-snippets
+  
+      # TODO: There doesn't appear to be a nix package for this:
+      # https://search.nixos.org/packages?channel=25.11&query=vimPlugins+nvim-markdown
+      # This came from:
+      # https://github.com/ixru/nvim-markdown
+      # There is a package for this other plugin:
+      # https://github.com/tadmccorkle/markdown.nvim
+      # So I'm installing that for now and will see if it suffices. It does have ~2.5
+      # times as many stars on GitHub so maybe it's even better
+      #nvim-markdown
+      #markdown.nvim
+      # TODO: This is also failing to install even though the internet claims
+      # this should be correct:
+      # https://search.nixos.org/packages?channel=25.11&show=vimPlugins.markdown-nvim&query=vimPlugins.markdown-nvim
+      # So I'm just removing for now. Maybe our Nix is out-of-date or something? Didn't spend much time debugging
+      #markdown-nvim
+  
+      vim-clang-format
+      nvim-lspconfig
+  
+      copilot-vim
+      
+      base16-vim
+      vim-puppet
+      vim-go
+      vim-csharp
+      emmet-vim
+      vim-vinegar
+      vim-flake8
+      vim-glsl
+      Jenkinsfile-vim-syntax
+      plantuml-syntax
+      # TODO: This isn't the right name in Nix but I use Cassandra so
+      # infrequently I'm just commenting this out. We can fix if we ever need
+      # it again.
+      #cql-vim
+      vim-fugitive
+      # TODO: Also doesn't appear to have a nix package yet
+      #mesonic
+  
+      nvim-cmp
+      lspsaga-nvim
+      lualine-nvim
+    ];
+  
+    extraPackages = with pkgs; [
+      fzf
+      ripgrep
+      fd
+      clang-tools
+      nodejs # needed for copilot + some LSP tooling
+    ];
+  };
+
+
   programs.home-manager.enable = true;
 
   # Optional: generate a static completion file during activation
