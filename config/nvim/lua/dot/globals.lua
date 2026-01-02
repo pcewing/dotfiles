46ae64117fdec10dadfe24f372ad4f145af739f9
Paul Ewing <pcewing00@gmail.com>
2026-01-02 00:59:56 -0800

    Remove vim-plug reference I missed

--- config/nvim/lua/dot/globals.lua
+++ config/nvim/lua/dot/globals.lua
@@ -1,7 +1,6 @@
 local vim = vim
 
 local Util    = require('dot.util')
-local VimPlug = require('dot.vim_plug')
 
 local M = {}
 
@@ -31,8 +30,6 @@ function M.init()
     _G.move_to_column             = Util.move_to_column
     _G.print_current_filetype     = Util.print_current_filetype
     _G.reload_config              = Util.reload_config
-
-    _G.install_vim_plug           = VimPlug.install
 
     M._create_commands({
         { 'ReloadConfig',     Util.reload_config,       {} },
