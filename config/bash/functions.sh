785c957b2b28c96399cfd7da1e4625860608adeb
Paul Ewing <pcewing00@gmail.com>
2026-01-03 03:06:47 -0800

    Start on git_diff_with function

--- config/bash/functions.sh
+++ config/bash/functions.sh
@@ -771,3 +771,77 @@ gh_clone()
 
     git clone "$url" "$path"
 }
+
+# TODO: Implement this
+# Diff the current git repository with a previous commit using Beyond Compare.
+# Copies the repository to a temp directory and checks out the specified
+# commit/branch/tag. Then runs Beyond compare to do a directory comparison
+# between the current directory and the temp directory.
+git_diff_with()
+{
+    # TODO: Document this parameter
+    local diff_target="$1"
+
+    if [ -z "$diff_target" ]; then
+        yell "ERROR: Missing argument <diff_target>"
+        return 1
+    fi
+
+    # TODO: We could make this robust and walk up the file tree but for now
+    # keep it simple
+    if [ ! -d "./.git" ]; then
+        yell "ERROR: Not at the root of a git repository"
+        return 1
+    fi
+
+    local tmp_dir_name tmp_dir
+    tmp_dir_name="$(basename $(pwd))_${diff_target}_diff"
+    tmp_dir="$HOME/tmp/${tmp_dir_name}"
+    
+    # TODO: DEBUG REMOVE
+    echo "tmp_dir = $tmp_dir"
+
+    # TODO: Error handling below
+
+    # Make sure the temporary directory doesn't already exist
+    # TODO: Maybe if it does just leave it and use it? If we're targeting a
+    # commit hash or tag it's unlikely to have changed. Could add a parameter
+    # to force re-checkout?
+    #rm -rf "$tmp_dir"
+
+    if [ ! -d "$tmp_dir" ]; then
+        # Make sure the parent directory exists
+        if ! mkdir -p "$(dirname "$tmp_dir")"; then
+            yell "ERROR: TODO0"
+            return 1
+        fi
+
+        if ! cp -r "$(pwd)" "$tmp_dir"; then
+            yell "ERROR: TODO1"
+            return 1
+        fi
+    fi
+
+    if ! cd "$tmp_dir"; then
+        yell "ERROR: TODO2"
+        return 1
+    fi
+
+    if ! git checkout "$diff_target"; then
+        yell "ERROR: TODO3"
+        return 1
+    fi
+
+    local bcompare_exe
+    # TODO: If WSL else...
+    #bcompare_exe="bcompare"
+    bcompare_exe="BCompare.exe"
+
+    # TODO: Doesn't work on WSL because we need to canonicalize the path for Windows
+    # e.g. /home/pewing/foo -> \\wsl.localhost\Ubuntu-24.04\home\pewing\foo
+    echo -e "Executing:\n\"$bcompare_exe\" \"$(pwd)\" \"$tmp_dir\""
+    #"$bcompare_exe" "$(pwd)" "$tmp_dir"
+    "$bcompare_exe" \
+        "\\\\WSL.LOCALHOST\\Ubuntu-24.04\\home\\pewing\\dot" \
+        "\\\\WSL.LOCALHOST\\Ubuntu-24.04\\home\\pewing\\tmp\\${tmp_dir_name}"
+}
